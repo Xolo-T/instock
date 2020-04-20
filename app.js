269ae9c9cc12dad5cb5ace35ef2cafaa48f561e8
Xolo <solomonmanyaga@gmail.com>
2020-04-20 08:30:27 -0400

    user route added

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("inStock coming very very Soon!"));
+const db = require("./config/keys").mongoURI;
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+    // console.log(res);
+    res.send("inStock coming very very Soon!");
+});
+
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 

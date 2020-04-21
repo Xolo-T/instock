41439e2000483fb5cac50c1b21a010d3d6bd8422
Xolo <solomonmanyaga@gmail.com>
2020-04-21 10:27:49 -0400

    report validations added

--- routes/api/reports.js
+++ routes/api/reports.js
@@ -4,11 +4,31 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Report = require('../../models/Report');
-// const validateReportInput = require('../..');
+const validateReportInput = require('../../validation/reports');
 
-router.get('/', (req, res) => {
+router.get('/test', (req, res) => {
     res.json({ msg: 'Welcome to reports'})
 });
+
+
+
+router.post('/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateTweetInput(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        const newTweet = new Tweet({
+            text: req.body.text,
+            user: req.user.id
+        });
+
+        newTweet.save().then(tweet => res.json(tweet));
+    }
+);
 
 
 module.exports = router;
---
+++ validation/reports.js
@@ -0,0 +1,23 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateReportInput(data) {
+    let errors = {};
+
+    data.placeId = validText(data.placeId) ? data.placeId : '';
+
+    if (Validator.isEmpty(data.placeId)) {
+        errors.placeId = 'placeId field is required';
+    }
+
+    data.productType = validText(data.productType) ? data.productType : '';
+
+    if (Validator.isEmpty(data.productType)) {
+        errors.productType = 'Product type field is required';
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    };
+};

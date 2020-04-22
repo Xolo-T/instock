f8db9f79f8911fd0c409bb68000661dca1cb1ff9
Xolo <solomonmanyaga@gmail.com>
2020-04-21 20:29:52 -0400

    report routes completed

--- models/User.js
+++ models/User.js
@@ -5,10 +5,12 @@ const UserSchema = new Schema({
   userName: {
     type: String,
     required: true,
+    unique:true
   },
   email: {
     type: String,
     required: true,
+    unique: true
   },
   password: {
     type: String,

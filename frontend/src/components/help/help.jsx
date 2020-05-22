3d2d34069dfa6c4f96a146b6a8845209caa10c84
jmkaneshiro <jmkaneshiro@gmail.com>
2020-05-22 12:43:58 -0400

    Update styling for modal, help, and about on smaller screens

--- frontend/src/components/help/help.jsx
+++ frontend/src/components/help/help.jsx
@@ -17,25 +17,31 @@ class help extends React.Component {
         <span className="login-or-signup-message">Instructions</span>
         
         <div className='help-instructions'>
+          <div>
             <h3>To view reports</h3>
-            <ul className='instruction-ul'>
-                <li>Simply click on a tp icon to view the details</li>
-                <li>You can also search for a store to see if it was reported</li>
+            <ul>
+              <li>Simply click on a tp icon to view the details</li>
+              <li>You can also search for a store to see if it was reported</li>
             </ul>
+          </div>
+          <div>
             <h3>To approve/disapprove reports</h3>
             <ul>
-                <li>Log in</li>
-                <li>Open report you want to approve</li>
-                <li>Click on thumbs up to approve or vice versa</li>
+              <li>Log in</li>
+              <li>Open report you want to approve</li>
+              <li>Click on thumbs up to approve or vice versa</li>
             </ul>
+          </div>
+          <div>
             <h3>To create reports</h3>
             <ul>
-                <li>Log in</li>
-                <li>Use the search bar to search for a store you want to report</li>
-                <li>If store isn't reported yet a form will pop up</li>
-                <li>You can edit the name if not accurate</li>
-                <li>Submit report by clicking "Yup"</li>
+              <li>Log in</li>
+              <li>Use the search bar to search for a store you want to report</li>
+              <li>If store isn't reported yet a form will pop up</li>
+              <li>You can edit the name if not accurate</li>
+              <li>Submit report by clicking "Yup"</li>
             </ul>
+          </div>
         </div>
       
       </div>
